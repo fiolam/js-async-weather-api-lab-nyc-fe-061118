dece2dd7a329864967f80e58d028962ad145dc33
fiolam <fiola.mulliqi@wework.com>
2018-08-08 22:15:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const API_KEY = "YOUR API KEY"
-
+const API_URL= "https://samples.openweathermap.org/data/2.5/forecast?id=524901&appid=b1b15e88fa797225412429c1c50c122a1"
 
 function handleFormSubmit(event) {
   
